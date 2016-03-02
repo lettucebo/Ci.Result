1905097eda7794b9669ec5d0d8691446470b0753
Money <abc12207@gmail.com>
2016-03-02 09:35:55 +0800

    feat: 完成第一版

---
+++ Creatidea.Library.Results/CiResult.cs
@@ -0,0 +1,39 @@
+﻿namespace Creatidea.Library.Results
+{
+    /// <summary>
+    /// 執行結果
+    /// </summary>
+    public class CiResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CiResult"/> class.
+        /// </summary>
+        public CiResult()
+        {
+            Success = false;
+        }
+
+        /// <summary>
+        /// 是否執行成功
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// 執行訊息
+        /// </summary>
+        public string Message { get; set; }
+    }
+
+    /// <summary>
+    /// 執行結果
+    /// </summary>
+    /// <typeparam name="T">泛型回傳</typeparam>
+    /// <seealso cref="CiResult" />
+    public class CiResult<T> : CiResult
+    {
+        /// <summary>
+        /// 結果物件
+        /// </summary>
+        public T Data { get; set; }
+    }
+}
