fe6c64d62526c3a62ecaa21766f0da0a8d9d3775
Money <abc12207@gmail.com>
2016-10-01 17:56:23 +0800

    correcttion result parameter name

--- Ci.Result/CiResult.cs
+++ Ci.Result/CiResult.cs
@@ -10,13 +10,13 @@
         /// </summary>
         public CiResult()
         {
-            Status = Result.Fail;
+            Result = Result.Fail;
         }
 
         /// <summary>
         /// 是否執行成功
         /// </summary>
-        public Result Status { get; set; }
+        public Result Result { get; set; }
 
         /// <summary>
         /// 執行訊息
