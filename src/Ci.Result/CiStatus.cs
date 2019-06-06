11e8ca82e075429e5322f68be2abd370c600a00b
Money Yu <abc12207@gmail.com>
2019-06-06 12:23:53 +0800

    adjust project structure

---
+++ src/Ci.Result/CiStatus.cs
@@ -0,0 +1,61 @@
+﻿namespace Ci.Result
+{
+    /// <summary>
+    /// Result code
+    /// </summary>
+    public enum CiStatus
+    {
+        /// <summary>
+        /// Everything went well
+        /// </summary>
+        Success = 1,
+
+        /// <summary>
+        /// Something went wrong
+        /// </summary>
+        Failure = 2,
+
+        /// <summary>
+        /// Can not find something
+        /// </summary>
+        NotFound = 3,
+
+        /// <summary>
+        /// Took too long to process something
+        /// </summary>
+        Timeout = 4,
+
+        /// <summary>
+        /// Same thing already exist
+        /// </summary>
+        Duplicate = 5,
+
+        Valid = 6,
+
+        Expired = 7,
+
+        Used = 8,
+
+        UnAuthorized = 9,
+
+        /// <summary>
+        /// database query error
+        /// </summary>
+        DbError = 10,
+
+        /// <summary>
+        /// parameter error
+        /// </summary>
+        ParameterError = 11,
+
+        /// <summary>
+        /// Model Validation Fail
+        /// </summary>
+        ValidationFail = 12,
+
+        /// <summary>
+        /// Data not being change
+        /// </summary>
+        NoChange = 13
+    }
+}
